5b4ba0ce0204a2761989e73f06783dea74a9ac52
aryanmalyala24 <aryanmalyala.24@kgpian.iitkgp.ac.in>
2026-06-13 20:39:36 +0530

    Initial commit

---
+++ roles.py
@@ -0,0 +1,42 @@
+INTERVIEW_ROLES = {
+    "software_engineer": {
+        "keywords": ["oop", "dsa", "data structures", "algorithms", "operating", "networks"],
+        "difficulty": "hard"
+    },
+    "data_analyst": {
+        "keywords": ["sql", "excel", "statistics", "python", "visualization"],
+        "difficulty": "medium"
+    },
+    "web_developer": {
+        "keywords": ["html", "css", "javascript", "react", "api"],
+        "difficulty": "medium"
+    },
+    "app_developer": {
+        "keywords": ["android", "ios", "flutter", "firebase", "api"],
+        "difficulty": "medium"
+    },
+    "cybersecurity_analyst": {
+        "keywords": ["cybersecurity", "networking", "linux", "owasp", "cryptography"],
+        "difficulty": "hard"
+    },
+    "cloud_engineer": {
+        "keywords": ["aws", "docker", "kubernetes", "networking", "ci", "cd"],
+        "difficulty": "hard"
+    },
+    "full_stack_developer": {
+        "keywords": ["frontend", "backend", "databases", "api", "cloud"],
+        "difficulty": "hard"
+    },
+    "data_scientist": {
+        "keywords": ["python", "statistics", "machine learning", "sql", "analytics"],
+        "difficulty": "hard"
+    },
+    "ai_engineer": {
+        "keywords": ["llm", "rag", "ai agents", "prompt", "mlops"],
+        "difficulty": "godlevel"
+    },
+    "database_engineer": {
+        "keywords": ["sql", "database", "design", "indexing", "transactions", "nosql"],
+        "difficulty": "medium"
+    }
+}
